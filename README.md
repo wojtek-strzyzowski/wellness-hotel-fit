784ff0623a9aa79077875911dc4920c665adcfd3
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-01-11 12:15:26 +0100

    grundaufbau der Seiten

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wellness-Hotel-Projekt
+Es handelt sich um einen Frage-Bogen (bestehend aus 12 Seiten).
+Siehe Figma: https://www.figma.com/file/DTEhdKdNBZq8um4k0qTG75/Gesundheitsfragebogen?type=design&node-id=0-1&mode=design&t=VxPLfK7KgKhcz4Ch-0
+
+1. Startseite mit Willkomen und Umfrage-Start
+2. gefolgt von 9 Frage Seiten
+3. Resultat- Seite
+4. Feedback-Seite
+
+## Projekt-Teilnehmer:
+Andreas Schollenberger
+Wojtek Strzyowski
+
+
+
---
+++ www/public/index.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wellness Hotel FIT</title>
+    <link href="./src/css/style.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="grid-container"> 
+    
+        <header class="header">Header</header>
+
+        <hero class="hero">hero</hero>
+
+        <main class="main">main</main>
+
+        <footer class="footer">footer</footer>
+
+</div>
+</body>
+</html>
